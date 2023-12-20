149bb2ad36afa88ccdbfdb57d7662ec48ac96078
ЯΛYИΛR <rayan.idri@edu.devinci.fr>
2023-12-20 15:54:39 +0100

    Display lesson list  on /lessons

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,19 +1,17 @@
-
 import ReactDOM from "react-dom/client";
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
-import Students from "./pages/Students.jsx";
+import Lessons from "./pages/Lessons.jsx";
 import Home from "./pages/Home.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
       <BrowserRouter>
         <Routes>
-          <Route path="/home" element = {<Home/>} />
-          <Route path="/students" element={<Students/>} />
+          <Route path="/" element={<Home/>} />
+          <Route path="/lessons" element={<Lessons/>} />
         </Routes>
         </BrowserRouter>
     </React.StrictMode>
 );
-
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -5,6 +5,7 @@ const Home = () => {
     <div>
       <h1>Page d'accueil</h1>
       <p>Bienvenue sur la page d'accueil !</p>
+      <button onClick={() => window.location.href="/lessons"}>Go to Lesson list</button>
     </div>
   );
 };
