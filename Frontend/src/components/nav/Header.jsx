022fd98111ce612479fa6706387c7a49b2bd2582
ЯΛYИΛR <rayan.idri@edu.devinci.fr>
2023-12-21 14:23:14 +0100

    assets added

--- Frontend/src/components/nav/Header.jsx
+++ Frontend/src/components/nav/Header.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import logo from "../../assets/IIM.png";
 
 const Header = () => {
   return (
     <nav className="comp-nav-header">
       <ul>
-        <li className="logo"><Link to="/"><span>4</span></Link></li>
+        <li className="logo">
+          <Link to="/">
+            <img src={logo} alt="Logo"/>
+          </Link>
+        </li>
         <li><Link to="/notes">Cours</Link></li>
         <li><Link to="/students">Étudiants</Link></li>
         <li><Link to="/speakers">Professeurs</Link></li>
       </ul>
     </nav>
-)
-  ;
+  );
 };
 
 export default Header;
