fb4f38f869d856ef9c79723ca0c58e406bd03faf
ЯΛYИΛR <rayan.idri@edu.devinci.fr>
2024-02-12 14:03:40 +0100

    mongoDB code moved to index.js

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -16,6 +16,8 @@ import Header from "./components/nav/Header.jsx";
 import Layout from "./components/Layout"
 import Profile  from "./pages/Profil.jsx";
 
+const { mongoPassword } = process.env.mongoPassword;
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <div>
     <React.StrictMode>
@@ -38,3 +40,31 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     </React.StrictMode>
   </div>
 );
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://noteIIMadmin:" + mongoPassword + "@clusternoteiim.pmtr9im.mongodb.net/?retryWrites=true&w=majority";
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
