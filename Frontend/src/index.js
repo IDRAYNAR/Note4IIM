9d8788785692316e17edde4963d85eab1e44b778
ЯΛYИΛR <rayan.idri@edu.devinci.fr>
2024-02-12 14:31:18 +0100

    axios installed and mongoDB code updated

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -15,8 +15,7 @@ import Speakers from "./pages/Speakers.jsx";
 import Header from "./components/nav/Header.jsx";
 import Layout from "./components/Layout"
 import Profile  from "./pages/Profil.jsx";
-
-const { mongoPassword } = process.env.mongoPassword;
+import axios from "axios";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <div>
@@ -41,30 +40,30 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   </div>
 );
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://noteIIMadmin:" + mongoPassword + "@clusternoteiim.pmtr9im.mongodb.net/?retryWrites=true&w=majority";
-
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+var data = JSON.stringify({
+    "collection": "Lessons",
+    "database": "Note4IIM_Data",
+    "dataSource": "ClusterNoteIIM",
+    "projection": {
+        "_id": 1
+    }
 });
-
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+            
+var config = {
+    method: 'post',
+    url: 'https://eu-central-1.aws.data.mongodb-api.com/app/data-azgpq/endpoint/data/v1/action/findOne',
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Request-Headers': '*',
+      'api-key': 'uTqnkTiYPHsR0azbCd2U4VucDd8kcltHq4MFpnwPgZ732FzhNRUDsLIPCeIDLenB',
+    },
+    data: data
+};
+            
+axios(config)
+    .then(function (response) {
+        console.log(JSON.stringify(response.data));
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
