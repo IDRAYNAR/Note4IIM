72315a9c1d511fdb43cfc79bff378a4b65132a95
Alexis Chochina <alexis@fcinq.com>
2024-02-13 15:46:50 +0100

    début migration

---
+++ frontend-next/src/app/components/card.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Link from 'next/link';
+
+interface CardProps {
+    className?: string;
+    target?: string;
+    title: string;
+    description?: string;
+    link: string;
+}
+
+const Card: React.FC<CardProps> = ({ className, target, title, description, link }) => {
+    return (
+        <Link href={link} legacyBehavior>
+            <a target={target} className={`comp-card ${className}`}>
+                <h3>{title}</h3>
+                {description ? <p>{description}</p> : null}
+            </a>
+        </Link>
+    );
+};
+
+export default Card;
---
+++ frontend-next/src/app/etudiants/page.tsx
@@ -0,0 +1,44 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = process.env.REACT_APP_SUPALINK;
+const supabaseAnonKey = process.env.REACT_APP_SUPAKEY;
+
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+interface Student {
+    name: string;
+    email: string;
+    cursus: string;
+    year: number;
+}
+
+export default async function etudiants() {
+    let { data: studentsData, error } = await supabase
+        .from<Student>('N4I_Students')
+        .select('name, email, cursus, year');
+
+    if (error) {
+        return <div>An error occured: {error.message}</div>;
+    }
+
+    const students = studentsData || [];
+
+    return (
+        <div className="App students wrapper -medium">
+            <div className="personListContainer">
+                {students.map((student) => (
+                    <div key={student.email} className="personCard">
+                        <p>
+                            <b>{student.name.toUpperCase()}</b>
+                        </p>
+                        <code>{student.email}</code>
+                        <div className="more-infos">
+                            <p>Cursus: <b>{student.cursus}</b></p>
+                            <p>Année: <b>{student.year}</b></p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
