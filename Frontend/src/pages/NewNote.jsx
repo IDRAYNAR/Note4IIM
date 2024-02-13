079bf860bdbe22b5a2e0a45b5c3010e80d21045e
fullmc <lambomarieclaire@yahoo.fr>
2024-02-13 13:59:06 +0100

    switched for supabase doc

--- Frontend/src/pages/NewNote.jsx
+++ Frontend/src/pages/NewNote.jsx
@@ -1,75 +1,102 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import { supabase } from "../supabase";
+// import axios from "axios";
 
 const NewNote = () => {
-  const [error, setError] = useState(null);
-  const [redirect, setRedirect] = useState(false);
-  const [lessonId, setLessonId] = useState(null);
+	const [error, setError] = useState(null);
+	const [redirect, setRedirect] = useState(false);
+	const [lessonId, setLessonId] = useState(null);
 
-  const createNewLesson = (event) => {
-    event.preventDefault();
-    const form = event.target;
-    const nom = form.nom.value;
-    const notes = form.notes.value;
+	const createNewLesson = async (event) => {
+		event.preventDefault();
+		const form = event.target;
+		const nom = form.nom.value;
+		const notes = form.notes.value;
 
-    axios.get("http://localhost:1337/api/students")
-    .then((response) => {
-      const students = response.data.data;
-      const randomIndex = Math.floor(Math.random() * students.length);
-      const selectedStudent = students[randomIndex];
+		try {
+			const { data, error } = await supabase
+				.from("N4I_Lessons")
+				.insert([{ name: nom, content: notes }])
+				.select();
 
-        axios.post("http://localhost:1337/api/lessons", {
-          data: {
-            Nom: nom,
-            Notes: notes,
-            Auteur: selectedStudent.id
-          }
-        })
-          .then(({ data }) => {
-            if (data && data.data && data.data.id) {
-              setLessonId(data.data.id);
-              setRedirect(true);
-            } else {
-              console.error("Received invalid lesson data");
-            }
-          })
-          .catch((error) => {
-            if (error.response) {
-              console.error("Error response from server:", error.response.data);
-            }
-            setError(error);
-          });
-      })
-      .catch((error) => {
-        console.error("Error fetching student:", error);
-        setError(error);
-      });
-  };
+			if (error) {
+				console.error("Erreur lors de l'insertion :", error.message);
+				setError(error);
+			} else {
+				if (data && data[0] && data[0].id) {
+					setLessonId(data[0].id);
+					setRedirect(true);
+				} else {
+					console.error("Received invalid lesson data");
+				}
+			}
+		} catch (error) {
+			console.error("Error response from server:", error.message);
+			setError(error);
+		}
 
-  useEffect(() => {
-    if (redirect && lessonId) {
-      window.location.href = `/single-note/${lessonId}`;
-    }
-  }, [redirect, lessonId]);
+		// axios
+		// 	.get("http://localhost:1337/api/students")
+		// 	.then((response) => {
+		// 		const students = response.data.data;
+		// 		const randomIndex = Math.floor(Math.random() * students.length);
+		// 		const selectedStudent = students[randomIndex];
 
-  if (error) {
-    return <div>Une erreur s'est produite : {error.message}</div>;
-  }
+		// 		axios
+		// 			.post("http://localhost:1337/api/lessons", {
+		// 				data: {
+		// 					Nom: nom,
+		// 					Notes: notes,
+		// 					Auteur: selectedStudent.id,
+		// 				},
+		// 			})
+		// 			.then(({ data }) => {
+		// 				if (data && data.data && data.data.id) {
+		// 					setLessonId(data.data.id);
+		// 					setRedirect(true);
+		// 				} else {
+		// 					console.error("Received invalid lesson data");
+		// 				}
+		// 			})
+		// 			.catch((error) => {
+		// 				if (error.response) {
+		// 					console.error("Error response from server:", error.response.data);
+		// 				}
+		// 				setError(error);
+		// 			});
+		// 	})
+		// 	.catch((error) => {
+		// 		console.error("Error fetching student:", error);
+		// 		setError(error);
+		// 	});
+	};
 
-  return (
-    <div className="new-note wrapper -medium">
-      <h1>Créer une nouvelle note</h1>
+	useEffect(() => {
+		if (redirect && lessonId) {
+			window.location.href = `/single-note/${lessonId}`;
+		}
+	}, [redirect, lessonId]);
 
-      <form className="form" onSubmit={createNewLesson}>
-        <label htmlFor="nom">Nom du cours</label>
-        <input className="input-title" type="text" id="nom" name="nom" />
+	if (error) {
+		return <div>Une erreur s'est produite : {error.message}</div>;
+	}
 
-        <label htmlFor="notes">Notes</label>
-        <textarea className="input-notes" id="notes" name="notes"></textarea>
-        <button className="update-btn" type="submit">Créer un nouveau cours</button>
-      </form>
-    </div>
-  );
+	return (
+		<div className="new-note wrapper -medium">
+			<h1>Créer une nouvelle note</h1>
+
+			<form className="form" onSubmit={createNewLesson}>
+				<label htmlFor="nom">Nom du cours</label>
+				<input className="input-title" type="text" id="nom" name="nom" />
+
+				<label htmlFor="notes">Notes</label>
+				<textarea className="input-notes" id="notes" name="notes"></textarea>
+				<button className="update-btn" type="submit">
+					Créer un nouveau cours
+				</button>
+			</form>
+		</div>
+	);
 };
 
 export default NewNote;
--- Frontend/src/pages/SingleNote.jsx
+++ Frontend/src/pages/SingleNote.jsx
@@ -4,31 +4,33 @@ import axios from "axios";
 import ReactMarkdown from "react-markdown";
 
 const SingleNote = () => {
-  const { id } = useParams();
-  const [note, setNote] = useState(null);
+	const { id } = useParams();
+	const [note, setNote] = useState(null);
 
-  useEffect(() => {
-    axios
-      .get(`http://localhost:1337/api/lessons/${id}?populate=*`)
-      .then(({ data }) => setNote(data.data))
-      .catch((error) => console.error("Error fetching note:", error));
-  }, [id]);
+	useEffect(() => {
+		axios
+			.get(`http://localhost:1337/api/lessons/${id}?populate=*`)
+			.then(({ data }) => setNote(data.data))
+			.catch((error) => console.error("Error fetching note:", error));
+	}, [id]);
 
-  if (!note) {
-    return <div>Loading...</div>;
-  }
+	if (!note) {
+		return <div>Loading...</div>;
+	}
 
-  return (
-    <div className="containerElement wrapper -medium single-note">
-      <h1>{note.attributes.Nom}</h1>
-      <hr />
-      <div className="lessonContainer">
-        <ReactMarkdown children={note.attributes.Notes} />
-      </div>
-      <hr />
-      <Link className="btn-edit" to={`/edit-note/${note.id}`}>Modifier la note</Link>
-    </div>
-  );
+	return (
+		<div className="containerElement wrapper -medium single-note">
+			<h1>{note.attributes.Nom}</h1>
+			<hr />
+			<div className="lessonContainer">
+				<ReactMarkdown children={note.attributes.Notes} />
+			</div>
+			<hr />
+			<Link className="btn-edit" to={`/edit-note/${note.id}`}>
+				Modifier la note
+			</Link>
+		</div>
+	);
 };
 
 export default SingleNote;
