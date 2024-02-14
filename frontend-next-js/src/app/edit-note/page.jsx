710270897890c996afa1f31900c71c602c480d42
Alexis Chochina <alexis@fcinq.com>
2024-02-14 16:39:50 +0100

    edit-note

--- frontend-next-js/src/app/edit-note/page.jsx
+++ frontend-next-js/src/app/edit-note/page.jsx
@@ -1,48 +1,77 @@
 "use client"
 
-//import React, { useEffect, useState } from "react";
-//import { useParams, useNavigate } from "react-router-dom";
 import React from "react";
 import {useSearchParams} from "next/navigation";
-import Link from "next/link";
-import ReactMarkdown from "react-markdown";
 import {supabase} from "@/supabase";
-import { useEffect, useState } from "react";
-import axios from "axios";
+import {useEffect, useState} from "react";
+import {useRouter} from 'next/navigation';
+
 
 const EditNote = () => {
-    const navigate = useNavigate();
-    const { id } = useParams();
+
+    const router = useRouter(); // Add this line
+
     const [note, setNote] = useState(null);
-    const [lessons, setLessons] = useState([]);
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
+    const searchParams = useSearchParams();
+    const id = searchParams.get("id");
 
     useEffect(() => {
-        axios
-                .get(`http://localhost:1337/api/lessons/${id}`)
-                .then(({ data }) => setNote(data.data))
-                .catch((error) => console.error("Error fetching note:", error));
+        const fetchNote = async () => {
+            let {data, error} = await supabase
+                    .from('N4I_Lessons')
+                    .select('*')
+                    .eq('id', id)
+
+            if (error) {
+                console.error("Error fetching note:", error)
+            } else {
+                if (data && data.length > 0) {
+                    setNote(data[0]);
+                    setTitle(data[0].name);
+                    setContent(data[0].content);
+                } else {
+                    console.error("No note found with id:", id)
+                }
+            }
+        }
+
+        fetchNote().then(r => r)
     }, [id]);
 
-    const updateLesson = (event, lessonId) => {
-        event.preventDefault();
-        const form = event.target;
-        const nom = form.nom.value;
-        const notes = form.notes.value;
+    const handleTitleChange = (event) => {
+        setTitle(event.target.value);
+    };
 
-        axios.put(`http://localhost:1337/api/lessons/${lessonId}`, {
-            data: {
-                Nom: nom,
-                Notes: notes
+    const handleContentChange = (event) => {
+        setContent(event.target.value);
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        const {error} = await supabase
+                .from('N4I_Lessons')
+                .update({name: title, content})
+                .eq('id', id);
+
+        if (error) {
+            console.error("Error updating note:", error);
+        } else {
+            const {data, error} = await supabase
+                    .from('N4I_Lessons')
+                    .select('*')
+                    .eq('id', id);
+
+            if (error) {
+                console.error("Error fetching updated note:", error);
+            } else {
+                setNote(data[0]);
+                router.push(`/single-note?id=${id}`);
             }
-        })
-                .then(({ data }) => {
-                    setLessons(lessons.map(lesson => lesson.id === data.id ? data : lesson));
-                    navigate('/notes');
-                })
-                .catch((error) => {
-                    console.error("Error updating lesson:", error);
-                });
-    }
+        }
+    };
 
     if (!note) {
         return <div>Loading...</div>;
@@ -51,12 +80,14 @@ const EditNote = () => {
     return (
             <div className="edit-note wrapper -medium">
                 <h1>Page d'édition d'une note</h1>
-                <form className="form" onSubmit={(event) => updateLesson(event, id)}>
+                <form className="form" onSubmit={handleSubmit}>
                     <label htmlFor="nom">Nom du cours</label>
-                    <input className="input-title" type="text" id="nom" name="nom" defaultValue={note?.attributes?.Nom} />
+                    <input className="input-title" type="text" id="title" name="title" defaultValue={note.name}
+                           onChange={handleTitleChange}/>
 
                     <label htmlFor="notes">Notes</label>
-                    <textarea className="input-notes" id="notes" name="notes" defaultValue={note?.attributes?.Notes}></textarea>
+                    <textarea className="input-notes" id="notes" name="notes" defaultValue={note.content}
+                              onChange={handleContentChange}/>
                     <button className="update-btn" type="submit">Mettre à jour</button>
                 </form>
             </div>
