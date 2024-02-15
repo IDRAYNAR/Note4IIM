374ff4675c72f791ec1d76591b6c25a19a99005e
RAYAN IDRI <ridri@bouyguestelecom.fr>
2024-02-15 15:58:11 +0100

    API Data moved from public to .env - Try 5

--- frontend-next-js/src/supabase.js
+++ frontend-next-js/src/supabase.js
@@ -1,9 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Use environment variables
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
 
+const supabaseUrl = 'https://lkhayewnehhxhqkckehs.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxraGF5ZXduZWhoeGhxa2NrZWhzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDI4OTc5NTMsImV4cCI6MjAxODQ3Mzk1M30.fKfRK_Dmx4KZ39za2coUrQGBbLJkrCbK_YIKEBbEQ6U'
+;
 const supabase = createClient(supabaseUrl, supabaseKey);
+
 
 export { supabase };
